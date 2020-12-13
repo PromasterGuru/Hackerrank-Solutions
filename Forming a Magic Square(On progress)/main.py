7bdae35abac98da5ed42da46e807d51591e33d8e
pmutondo12@gmail.com <=>
2020-12-13 12:24:13 +0300

    Angry Professor

--- Forming a Magic Square(On progress)/main.py
+++ Forming a Magic Square(On progress)/main.py
@@ -1,9 +1,20 @@
 def formingMagicSquare(s):
     cost = 0
-    for a in s:
-        for j in range(len(a)):
-            print(a[j])
+    #Fix diagnols
+    for x,ar in enumerate(s):
+        d = abs(15 - sum(ar))
+        if d > 0:
+            m = max(ar)
+            for y,j in enumerate(ar):
+                if j ==m:
+                    cost += abs(j-d)
+                    print(x,y)
+                    s[x][y] = d
+    print(s)
+    
+    return cost
 
+    
 if __name__ == "__main__":
     s = [
         [5, 3, 4],
@@ -11,7 +22,7 @@ if __name__ == "__main__":
         [6, 4, 2]
     ]
 
-    s= [
+    s = [
         [4, 9, 2],
         [3, 5, 7],
         [8, 1, 5]
@@ -23,3 +34,31 @@ if __name__ == "__main__":
         [6, 1, 6]
     ]
     print(formingMagicSquare(s))
+
+    """
+    [2 9 4]
+    [7 5 3]
+    [6 1 8]
+
+    [6 7 2]
+    [1 5 9]
+    [8 3 4]
+
+    [4 9 2]
+    [3 5 7]
+    [8 1 6]
+
+    [8 1 6]
+    [3 5 7]
+    [4 9 2]
+
+    [6 1 8]
+    [7 5 3]
+    [2 9 4]
+
+    (0,0) -> (0,1)(0,2)(2,2)
+    (1,1) -> (1,2)
+    (0,2) -> (0,0)(2,2)(0,1)
+    (1,2) -> (1,0)
+    (2,2) -> (0,0)(0,2)(1,2)
+    """
